957662ff407906b46bb996dfc2cde253d8d71c0f
Denis Korchagin <denis.korchagin.1995@gmail.com>
2020-09-26 18:51:54 +0300

    implement block memory allocator

--- allocator.c
+++ allocator.c
@@ -1,5 +1,74 @@
 #include "allocator.h"
 
+void * allocate(struct allocator * allocator, unsigned int nbytes)
+{
+	struct allocator_chunk_entry * chunk;
+	unsigned int alignment;
+
+	chunk = allocator->chunks;
+	alignment = allocator->alignment;
+
+	nbytes = (nbytes + alignment - 1) & ~(alignment - 1);
+
+	if(nbytes > allocator->chunk_size) {
+		fprintf(stderr, "error: tries to allocate too many bytes for '%s' allocator!\n", allocator->name);
+		exit(EXIT_FAILURE);
+	}
+
+	if(!chunk || nbytes > chunk->remains) {
+		struct allocator_chunk_entry * new_chunk = (struct allocator_chunk_entry *) malloc(allocator->chunk_size);
+		if(!new_chunk) {
+			fprintf(stderr, "error: out of memory!\n");
+			exit(EXIT_FAILURE);
+		}
+		size_t struct_member_offset = (size_t) &((struct allocator_chunk_entry *) 0)->memory;
+		unsigned int offset = ((unsigned int) struct_member_offset + alignment - 1) & ~(alignment - 1);
+		new_chunk->offset = offset - struct_member_offset;
+		new_chunk->remains = allocator->chunk_size - offset;
+		new_chunk->next = allocator->chunks;
+		allocator->chunks = new_chunk;
+
+		chunk = new_chunk;
+	}
+
+	void * memory = (void *)chunk->memory + chunk->offset;
+	chunk->offset += nbytes;
+	chunk->remains -= nbytes;
+	return memory;
+}
+
+void deallocate(struct allocator * allocator)
+{
+	struct allocator_chunk_entry * chunk, * next;
+
+	chunk = allocator->chunks;
+
+	allocator->chunks = NULL;
+	allocator->free_objects = NULL;
+
+	while(chunk) {
+		next = chunk->next;
+		free((void *) chunk);
+		chunk = next;
+	}
+}
+
+void * alloc_object(struct allocator * allocator, unsigned int size)
+{
+	if(allocator->free_objects) {
+		void ** entry = (void **) allocator->free_objects;
+		allocator->free_objects = *entry;
+		return (void *) entry;
+	}
+	return allocate(allocator, size);
+}
+
+void free_object(struct allocator * allocator, void * object)
+{
+	void ** entry = (void **) object;
+	*entry = allocator->free_objects;
+	allocator->free_objects = (void *) entry;
+}
 
 declare_allocator(fsm_state, struct fsm_state, 1024)
 declare_allocator(fsm, struct fsm, 16)
--- allocator.h
+++ allocator.h
@@ -9,6 +9,31 @@
 #include "parser_types.h"
 #include "character_list.h"
 
+
+struct allocator_chunk_entry
+{
+	struct allocator_chunk_entry * next;
+	unsigned int offset;
+	unsigned int remains;
+	char memory[];
+};
+
+struct allocator
+{
+	const char * name;
+	struct allocator_chunk_entry * chunks;
+	void * free_objects;
+	unsigned int alignment;
+	unsigned int chunk_size;
+};
+
+extern void * allocate(struct allocator * allocator, unsigned int nbytes);
+extern void deallocate(struct allocator * allocator);
+
+extern void * alloc_object(struct allocator * allocator, unsigned int size);
+
+extern void free_object(struct allocator * allocator, void * object);
+
 #define define_allocator(name, type)      \
   extern type * ___alloc_ ## name(void);  \
   extern uint32_t name ## _pool_cursor;   \
